a305c1689fca9a06cf972bd1fbcb515d1bab4234
Patrick Hannan <patrick@hannan.com>
2020-11-02 00:50:02 -0800

    fixed styling

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 const Navbar = () => {
   return (
-    <div className="jumbotron text-center">
+    <div className="jumbotron text-center" id="jumbotron">
       <div className="container">
         <h2>Employee Directory</h2>
       </div>
