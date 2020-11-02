d5405f215e40cae9fdf46267cdffe5b66d965b67
Patrick Hannan <patrick@hannan.com>
2020-11-02 00:38:57 -0800

    added jumbotron

---
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Jumbotron.css";
+
+const Navbar = () => {
+  return (
+    <div className="jumbotron text-center">
+      <div className="container">
+        <h2>Employee Directory</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
