6407545a8809b8241c5a347167f8e4ea2cbe20bc
Patrick Hannan <patrick@hannan.com>
2020-11-02 00:50:59 -0800

    Added home const

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -2,3 +2,14 @@ import React from "react";
 import Jumbotron from "../../components/Jumbotron/Jumbotron";
 import Table from "../../components/Table/Table";
 
+const Home = () => {
+    return (
+      <>
+        <Jumbotron />
+        <div className="container">
+              <Table />
+        </div>
+      </>
+    );
+  };
+  export default Home;
